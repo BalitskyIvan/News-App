9766c63f98c93e73ef98643e362a14f5b9e64f10
lmallado <hoobchik@gmail.com>
2024-08-30 11:34:15 +0300

    Fix theme colors + add back arrow to details screen

--- app/src/main/java/balitsky/newsapp/presentation/detailsscreen/DetailsScreen.kt
+++ app/src/main/java/balitsky/newsapp/presentation/detailsscreen/DetailsScreen.kt
@@ -1,53 +1,91 @@
 package balitsky.newsapp.presentation.detailsscreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import coil.compose.AsyncImage
 
 @Composable
 fun DetailsScreen(
     id: String,
+    navController: NavController,
     viewModel: DetailsScreenViewModel = hiltViewModel()
 ) {
     viewModel.initWithId(id = id)
 
-    Column(
-        modifier = Modifier.fillMaxSize()
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.TopStart
     ) {
-        AsyncImage(
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(300.dp),
-            model = viewModel.urlImage.collectAsState().value,
-            contentScale = ContentScale.Crop,
-            contentDescription = "News Image"
-        )
+                .fillMaxSize()
+                .padding(top = 40.dp)
+        ) {
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp),
+                model = viewModel.urlImage.collectAsState().value,
+                contentScale = ContentScale.Crop,
+                contentDescription = "News Image"
+            )
 
-        Text(
-            modifier = Modifier
-                .padding(top = 10.dp)
-                .padding(horizontal = 16.dp),
-            text = viewModel.title.collectAsState().value,
-            style = MaterialTheme.typography.titleLarge
-        )
+            Text(
+                modifier = Modifier
+                    .padding(top = 10.dp)
+                    .padding(horizontal = 16.dp),
+                text = viewModel.title.collectAsState().value,
+                style = MaterialTheme.typography.titleLarge
+            )
 
-        Text(
+            Text(
+                modifier = Modifier
+                    .padding(top = 14.dp)
+                    .padding(horizontal = 16.dp),
+                text = viewModel.content.collectAsState().value,
+                style = MaterialTheme.typography.bodyLarge
+            )
+        }
+
+        Box(
             modifier = Modifier
-                .padding(top = 14.dp)
-                .padding(horizontal = 16.dp),
-            text = viewModel.content.collectAsState().value,
-            style = MaterialTheme.typography.labelSmall
-        )
+                .padding(start = 10.dp, top = 50.dp)
+                .size(50.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(color = MaterialTheme.colorScheme.primaryContainer)
+                .clickable {
+                    navController.popBackStack()
+                },
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+                contentDescription = "Back Arrow"
+            )
+        }
     }
 }
--- app/src/main/java/balitsky/newsapp/presentation/ui/theme/Theme.kt
+++ app/src/main/java/balitsky/newsapp/presentation/ui/theme/Theme.kt
@@ -1,27 +1,20 @@
 package balitsky.newsapp.presentation.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Color.White,
+    primaryContainer = Color(0xFF3D3D3D),
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = Color.Black,
     primaryContainer = Color.White,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
     background = Color(0xFFEEEEEE),
     surface = Color(0xFFFFFBFE),
     onPrimary = Color.White,
@@ -34,15 +27,9 @@ private val LightColorScheme = lightColorScheme(
 @Composable
 fun NewsAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
